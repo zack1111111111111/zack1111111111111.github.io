8426dfed5069ce682cc6f5243fb10d1c13d39ff7
zack1111111111111 <zj28@njit.edu>
2026-05-07 05:54:15 +0000

    Deploying to gh-pages from @ zack1111111111111/zack1111111111111.github.io@ac292f506c539ec33f534c98d2383abbadf92e69 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%7A%6A%32%38@%6E%6A%69%74.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -539,7 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=g-TlPlgAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
